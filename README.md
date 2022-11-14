9a36581e173005372144a164abbf09d5f0267fff
ionov-e <inv.great@gmail.com>
2022-11-14 18:15:43 +0300

    v.1

---
+++ README.md
@@ -0,0 +1,20 @@
+# Simple currency converter (CBR)
+
+## Short description
+
+The whole point was to publish a library on packagist: https://packagist.org/packages/ioncurly/cbr-converter
+It uses CBR's XML file with currency rates
+
+This XML file (updated daily) is used: https://www.cbr.ru/scripts/XML_daily.asp
+
+## Usage
+
+`$exchange = new Exchange("USD", "UAH", 100);`
+
+`echo $exchange->toDecimal();`
+
+It would print some number like "**3,693.18**"
+
+* First parameter: short abbreviation (3 letters) of currency being converted from;
+* First parameter: short abbreviation (3 letters) of currency being converted to;
+* Third parameter: amount of money to be converted
--- src/Exchange.php
+++ src/Exchange.php
@@ -6,30 +6,56 @@ class Exchange
 {
     const URL_CURRENCY_RATE_XML = 'https://www.cbr.ru/scripts/XML_daily.asp';
 
-    private string $originalCurrency;
-
-    private string $targetCurrency;
-
     private int $amount;
-
-    private float $currencyRate;
-
+    private string $originalCurrencyName;
+    private string $targetCurrencyName;
+    private float $originalCurrencyRateToRub;
+    private float $targetCurrencyRateToRub;
     private float $result;
 
-    public function __construct(string $originalCurrency, string $targetCurrency, int $amount)
+    /** @throws \Exception */
+    public function __construct(string $originalCurrencyName, string $targetCurrencyName, int $amount)
     {
-        $this->originalCurrency = $originalCurrency;
-        $this->targetCurrency = $targetCurrency;
+        $this->originalCurrencyName = $originalCurrencyName;
+        $this->targetCurrencyName = $targetCurrencyName;
         $this->amount = $amount;
+        $this->setCurrencyRates();
+        $this->result = $this->getResult();
     }
 
-    public function getResult() // #TODO change to private
+    /** @throws \Exception */
+    private function setCurrencyRates(): void
     {
         $xml = simplexml_load_file(self::URL_CURRENCY_RATE_XML);
+
+        if (!$xml) {
+            throw new \Exception("Couldn't access XML with currency rates: " . self::URL_CURRENCY_RATE_XML);
+        }
+
+        foreach ($xml->Valute as $currency) {
+            if ($currency->CharCode == $this->originalCurrencyName) {
+                $currency->Value = (float)str_replace(',', '.', $currency->Value);
+                $currency->Nominal = (float)str_replace(',', '.', $currency->Nominal);
+                $this->originalCurrencyRateToRub = $currency->Value / $currency->Nominal;
+            } elseif ($currency->CharCode == $this->targetCurrencyName) {
+                $currency->Value = (float)str_replace(',', '.', $currency->Value);
+                $currency->Nominal = (float)str_replace(',', '.', $currency->Nominal);
+                $this->targetCurrencyRateToRub = $currency->Value / $currency->Nominal;
+            };
+        };
+
+        if (empty($this->originalCurrencyRateToRub) || empty($this->targetCurrencyRateToRub)) {
+            throw new \Exception("Couldn't find at least one of the provided currencies ('$this->originalCurrencyName' & '$this->targetCurrencyName')");
+        }
     }
 
-    public function toDecimal(): float
+    private function getResult(): float
     {
-        return ($this->result);
+        return $this->amount * $this->originalCurrencyRateToRub / $this->targetCurrencyRateToRub;
+    }
+
+    public function toDecimal(): string
+    {
+        return number_format($this->result, 2);
     }
 }
